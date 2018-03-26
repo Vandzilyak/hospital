9dffcbe8bb66a8bdff88605636dccfa114725d9c
Sergiy Vandzilyak <sergiy.vandzilyak@gmail.com>
2018-03-26 14:26:50 +0300

    commit

--- src/main/java/com/hospital/controller/HomeController.java
+++ src/main/java/com/hospital/controller/HomeController.java
@@ -1,17 +1,13 @@
 package com.hospital.controller;
 
 import com.hospital.entities.Patient;
-import com.hospital.entities.User;
 import com.hospital.service.PatientService;
 import com.hospital.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
